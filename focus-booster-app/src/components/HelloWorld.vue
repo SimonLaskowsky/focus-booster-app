527856455479896132202c2ec6cda0ac7b36058c
Szymon Laskowski <simonlaskowsky@gmail.com>
2024-04-23 03:15:00 +0200

    first commit

---
+++ focus-booster-app/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1 {
+  text-align: center;
+}
+
+</style>
