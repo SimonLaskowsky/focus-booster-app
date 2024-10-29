db1008cb861cc779660dc6ac64f17411bcd974c2
Szymon Laskowski <simonlaskowsky@gmail.com>
2024-10-29 18:19:45 +0100

    Made tutrial icons disspear

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -114,7 +114,7 @@ const updateTimerDisplay = (timerValue) => {
   <Vue3Spline
     ref="splineApp"
     :scene="{
-      url: 'https://draft.spline.design/tehH5CDG6IqZK6td/scene.splinecode',
+      url: 'https://draft.spline.design/zgKB5jfgdpNz2I5O/scene.splinecode',
     }"
   />
 </template>
